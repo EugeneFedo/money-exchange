f662b01934f76166c070b765dc09a2969f64fdf6
EugeneFedo <eugenefed91@gmail.com>
2019-03-19 00:05:00 +0300

    task ready

--- src/index.js
+++ src/index.js
@@ -1,5 +1,78 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    let coins = {};
+    let halfs;
+    let quarters;
+    let dimes;
+    let nickels;
+    let pennies;
+    let remainder;
+    if (currency > 10000) {
+        coins.error = "You are rich, my friend! We don't have so much coins for exchange";
+    } else if (currency >= 50) {
+        halfs = Math.floor(currency / 50);
+        coins.H = halfs;
+        remainder = currency % 50;
+        if (remainder >= 25) {
+          quarters = Math.floor(remainder / 25);
+          coins.Q = quarters;
+          remainder = remainder % 25;
+        }
+        if (remainder >= 10) {
+            dimes = Math.floor(remainder / 10);
+            coins.D = dimes;
+            remainder = remainder % 10;
+        }
+        if (remainder >= 5) {
+              nickels = Math.floor(remainder / 5);
+              coins.N = nickels;
+              remainder = remainder % 5;
+        }
+        if (remainder >= 1) {
+                pennies = Math.floor(remainder / 1);
+                coins.P = pennies;
+    }} else if (currency >= 25) {
+          quarters = Math.floor(currency / 25);
+          coins.Q = quarters;
+          remainder = currency % 25;
+          if (remainder >= 10) {
+            dimes = Math.floor(remainder / 10);
+            coins.D = dimes;
+            remainder = remainder % 10;
+          }
+          if (remainder >= 5) {
+              nickels = Math.floor(remainder / 5);
+              coins.N = nickels;
+              remainder = remainder % 5;
+          }
+          if (remainder >= 1) {
+                pennies = Math.floor(remainder / 1);
+                coins.P = pennies;
+    }} else if (currency >= 10) {
+            dimes = Math.floor(currency / 10);
+            coins.D = dimes;
+            remainder = currency % 10;
+            if (remainder >= 5) {
+              nickels = Math.floor(remainder / 5);
+              coins.N = nickels;
+              remainder = remainder % 5;
+            }
+            if (remainder >= 1) {
+                pennies = Math.floor(remainder / 1);
+                coins.P = pennies;
+    }} else if (currency >= 5) {
+              nickels = Math.floor(currency / 5);
+              coins.N = nickels;
+              remainder = currency % 5;
+              if (remainder >= 1) {
+                pennies = Math.floor(remainder / 1);
+                coins.P = pennies;
+    }} else if (currency >= 1) {
+              pennies = Math.floor(currency / 1);
+              coins.P = pennies;
 }
+  return coins;
+}
+    
+
+
